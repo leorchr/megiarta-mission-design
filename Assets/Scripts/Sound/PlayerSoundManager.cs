e395f410a93ae5098a6122efe99842aac75a382c
Komediem <gbussola@artfx.fr>
2024-03-07 09:50:37 +0100

    [fix] Player Sound manager

--- Assets/Scripts/Sound/PlayerSoundManager.cs
+++ Assets/Scripts/Sound/PlayerSoundManager.cs
@@ -17,14 +17,14 @@ public class PlayerSoundManager : MonoBehaviour
 
     public void Step()
     {
-        if(ambientSoundManager.isOnIsland && grassStepSounds.Count != 0)
+        if(ambientSoundManager != null && ambientSoundManager.isOnIsland && grassStepSounds.Count != 0)
         {
             AudioClip clip = grassStepSounds[Random.Range(0, grassStepSounds.Count)];
             stepSource.clip = clip;
             stepSource.Play();
         }
 
-        else if(!ambientSoundManager.isOnIsland && rockStepSounds.Count != 0)
+        else if(ambientSoundManager != null && !ambientSoundManager.isOnIsland && rockStepSounds.Count != 0)
         {
             AudioClip clip = rockStepSounds[Random.Range(0, rockStepSounds.Count)];
             stepSource.clip = clip;
