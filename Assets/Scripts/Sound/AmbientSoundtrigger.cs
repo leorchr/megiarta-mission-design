74d7222f4287ba0c2942f022884c358c67a3314e
Komediem <gbussola@artfx.fr>
2024-03-07 11:04:53 +0100

    [fix] collisions rock

--- Assets/Scripts/Sound/AmbientSoundtrigger.cs
+++ Assets/Scripts/Sound/AmbientSoundtrigger.cs
@@ -11,6 +11,7 @@ public class AmbientSoundtrigger : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             ambientSoundManager.AmbienceChange();
+            print("saucisse");
         }
     }
 }
