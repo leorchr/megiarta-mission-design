3b2690343a00a9c58dbf65fdcfa9e28be50283e2
ArkanioGX <86797763+ArkanioGX@users.noreply.github.com>
2024-03-22 14:19:51 +0100

    Nerfed Mouse Sensitivity

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -104,6 +104,7 @@ public class CameraController : MonoBehaviour
     {
         lookVectorInput = callbackContext.ReadValue<Vector2>();
         lookVectorInput = cData.invertedLook ? lookVectorInput * -1 : lookVectorInput; //Invert input
+        if (InputManager.instance.getCurrentControlScheme() == ControlScheme.Keyboard) { lookVectorInput /= new Vector2(Screen.width, Screen.height); }
     }
 
     public void CameraChange(InputAction.CallbackContext context)
