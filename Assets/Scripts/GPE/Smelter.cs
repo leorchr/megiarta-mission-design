553b92e942e154b35a14dab87cbe6c13ba21233a
L3o <leogravillon@hotmail.fr>
2024-03-22 18:06:11 +0100

    [refactor] remove useless item reward

--- Assets/Scripts/GPE/Smelter.cs
+++ Assets/Scripts/GPE/Smelter.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Smelter : QuestInteractor
 {
-
-    public ItemData obsidianReward;
-    //private bool doOnce = false;
-
     public override void OnInteraction()
     {
         base.OnInteraction();
