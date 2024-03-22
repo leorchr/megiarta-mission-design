c722ea3becc727aa7593461ee81d6d7f4adcee4d
Gabriel Cochard <gcochard@artfx.fr>
2024-03-22 12:06:53 +0100

    [dev] Pause menu ui navigation

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,29 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject settingsPanel;
 
+    [SerializeField] private GameObject settingsButton;
+    [SerializeField] private GameObject exitButton;
+
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
         Cursor.visible = false;
+        Debug.Log("Resume Button");
     }
 
     public void Settings()
     {
         pauseMenu.SetActive(false);
         settingsPanel.SetActive(true);
+        exitButton.GetComponent<Button>().Select();
     }
 
     public void QuitSettings()
     {
         settingsPanel.SetActive(false);
         pauseMenu.SetActive(true);
+        settingsButton.GetComponent<Button>().Select();
+
     }
 
     public void ReturnToMenu()
